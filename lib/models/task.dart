e0b22e2833952e3a1ca3de143d3db6c151462bd3
yamariel <ariel.yamien@epitech.eu>
2026-07-22 22:22:37 +0000

    [ADD]: ajout de type de tâche

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,18 +1,19 @@
 import 'package:todo_list/enums/priority.dart';
 
 abstract class Task {
+  final String type;
   final int id;
   String title;
   Priority priority;
   DateTime? date;
   bool isCompleted;
-  Task(this.id, this.title,this.priority, this.date, this.isCompleted);
+  Task(this.type, this.id, this.title,this.priority, this.date, this.isCompleted);
 
   void showTask() {
     print("ID: $id");
     print("Titre: $title");
-    print("Priorité: $priority");
-    print("Date limite: $date");
+    print("Priorité: ${priority.name}");
+    print("Date limite: ${date?.toString()}");
     print("Terminée ?: $isCompleted");
   }
 }
