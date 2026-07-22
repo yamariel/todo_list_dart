8c1a6f8b0983e7e85dc645e80f991e8c09b9a03f
yamariel <ariel.yamien@epitech.eu>
2026-07-22 22:22:07 +0000

    [ADD]: ajout de valeur type de tâche

--- lib/models/normal_task.dart
+++ lib/models/normal_task.dart
@@ -2,14 +2,13 @@ import 'package:todo_list/enums/priority.dart';
 import 'package:todo_list/models/task.dart';
 
 class NormalTask extends Task {
-
   NormalTask(
     int id,
     String title,
     Priority priority,
     DateTime? date, {
     bool isCompleted = false,
-  }) : super(id, title, priority, date, isCompleted);
+  }) : super("normal", id, title, priority, date, isCompleted);
 
   @override
   void showTask() {
--- lib/models/urgent_task.dart
+++ lib/models/urgent_task.dart
@@ -2,15 +2,14 @@ import 'package:todo_list/enums/priority.dart';
 import 'package:todo_list/models/task.dart';
 
 class UrgentTask extends Task {
-
   UrgentTask(
     int id,
     String title,
     Priority priority,
     DateTime? date, {
     bool isCompleted = false,
-  }) : super(id, title, priority, date, isCompleted);
-  
+  }) : super("urgent", id, title, priority, date, isCompleted);
+
   @override
   void showTask() {
     print("==== TÂCHE URGENTE !! ====");
